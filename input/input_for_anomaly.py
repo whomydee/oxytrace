874cb70c1726a6f8a7dda11e5db7f2080ebbebe2
Shad Humydee <showmmos@gmail.com>
2025-11-18 12:38:20 +0600

    add sample input for anomaly detection

---
+++ input/input_for_anomaly.py
@@ -0,0 +1,14 @@
+"""
+Input data for anomaly detection
+
+Edit this file with your oxygen sensor readings.
+You can provide data in two formats:
+"""
+
+input_data = [
+    {'time': '2024-01-01 10:00:00', 'Oxygen[%sat]': 88.5},
+    {'time': '2024-01-01 10:01:00', 'Oxygen[%sat]': 87.2},
+    {'time': '2024-01-01 10:02:00', 'Oxygen[%sat]': 89.1},
+    {'time': '2024-01-01 10:03:00', 'Oxygen[%sat]': 45.0},  # Example anomaly
+    {'time': '2024-01-01 10:04:00', 'Oxygen[%sat]': 88.0},
+]
